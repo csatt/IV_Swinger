21c9567497f1ec13f30aa56997bfe3ad7aa62b2b
Chris Satterlee <csatt1@gmail.com>
2020-10-22 15:20:01 -0700

    Add support for Python 3 to run_pyi.bat (Issue #121)
    The run_pyi.bat script now requires either "python" or "python3" as an
    argument to determine whether to build the Python 2 or Python 3 code.

--- build_tools/run_pyi.bat
+++ build_tools/run_pyi.bat
@@ -1,9 +1,19 @@
-pyinstaller --windowed ^
-            --noconfirm ^
-            --icon="%GITHUB%\IV_Swinger\icons\IV_Swinger2.ico" ^
-            --add-data="%GITHUB%\IV_Swinger\python\Splash_Screen.png;." ^
-            --add-data="%GITHUB%\IV_Swinger\python\Blank_Screen.png;." ^
-            --add-data="%GITHUB%\IV_Swinger\python\version.txt;." ^
-            --add-data="%GITHUB%\IV_Swinger\icons\IV_Swinger2.ico;." ^
-            --name "IV Swinger 2" ^
-            %GITHUB%\IV_Swinger\python\IV_Swinger2_gui.py
+@echo off
+if "%~1" NEQ "" (
+  if "%~1" == "python" set ok=1
+  if "%~1" == "python3" set ok=1
+)
+if defined ok (
+  pyinstaller --windowed ^
+              --noconfirm ^
+              --icon="%GITHUB%\IV_Swinger\icons\IV_Swinger2.ico" ^
+              --add-data="%GITHUB%\IV_Swinger\%1\Splash_Screen.png;." ^
+              --add-data="%GITHUB%\IV_Swinger\%1\Blank_Screen.png;." ^
+              --add-data="%GITHUB%\IV_Swinger\%1\version.txt;." ^
+              --add-data="%GITHUB%\IV_Swinger\icons\IV_Swinger2.ico;." ^
+              --name "IV Swinger 2" ^
+              %GITHUB%\IV_Swinger\%1\IV_Swinger2_gui.py
+) else (
+  echo ERROR: must specify either 'python' or 'python3'
+  exit -1
+)
