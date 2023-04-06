e8f83c97542bb841b9c682df86ee2ae1fbfb5f76
Chris Satterlee <csatt1@gmail.com>
2023-04-05 17:15:44 -0700

    Use plistlib instead of biplist
    Replace the no-longer-working biplist library for reading the Info.plist file with plistlib. This is now in http://dmgbuild.readthedocs.io/en/latest/example.html
    
    Also remove a couple unnecessary imports.

--- build_tools/dmgbuild_settings.py
+++ build_tools/dmgbuild_settings.py
@@ -4,11 +4,8 @@
 # Modified from http://dmgbuild.readthedocs.io/en/latest/example.html
 #
 ###############################################################################
-from __future__ import print_function
-
-import biplist
 import os.path
-import sys
+import plistlib
 
 #
 # Example settings file for dmgbuild
@@ -29,7 +26,8 @@ appname = os.path.basename(application)
 
 def icon_from_app(app_path):
     plist_path = os.path.join(app_path, 'Contents', 'Info.plist')
-    plist = biplist.readPlist(plist_path)
+    with open(plist_path, "rb") as f:
+        plist = plistlib.load(f)
     icon_name = plist['CFBundleIconFile']
     icon_root, icon_ext = os.path.splitext(icon_name)
     if not icon_ext:
