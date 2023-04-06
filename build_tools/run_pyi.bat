0132f529235b4ad2567c00a98888d52cab40838d
Chris Satterlee <csatt1@gmail.com>
2023-04-05 17:19:42 -0700

    Remove support for python 2
    Since Python 3 support was introduced, the mac_run_pyi and run_pyi.bat scripts required an argument of "python" or "python3". Now Python 2 is no longer supported, so the scripts are simplified to assume python3.

--- build_tools/run_pyi.bat
+++ build_tools/run_pyi.bat
@@ -1,19 +1,10 @@
 @echo off
-if "%~1" NEQ "" (
-  if "%~1" == "python" set ok=1
-  if "%~1" == "python3" set ok=1
-)
-if defined ok (
-  pyinstaller --windowed ^
-              --noconfirm ^
-              --icon="%GITHUB%\IV_Swinger\icons\IV_Swinger2.ico" ^
-              --add-data="%GITHUB%\IV_Swinger\%1\Splash_Screen.png;." ^
-              --add-data="%GITHUB%\IV_Swinger\%1\Blank_Screen.png;." ^
-              --add-data="%GITHUB%\IV_Swinger\%1\version.txt;." ^
-              --add-data="%GITHUB%\IV_Swinger\icons\IV_Swinger2.ico;." ^
-              --name "IV Swinger 2" ^
-              %GITHUB%\IV_Swinger\%1\IV_Swinger2_gui.py
-) else (
-  echo ERROR: must specify either 'python' or 'python3'
-  exit -1
-)
+pyinstaller --windowed ^
+            --noconfirm ^
+            --icon="%GITHUB%\IV_Swinger\icons\IV_Swinger2.ico" ^
+            --add-data="%GITHUB%\IV_Swinger\python3\Splash_Screen.png;." ^
+            --add-data="%GITHUB%\IV_Swinger\python3\Blank_Screen.png;." ^
+            --add-data="%GITHUB%\IV_Swinger\python3\version.txt;." ^
+            --add-data="%GITHUB%\IV_Swinger\icons\IV_Swinger2.ico;." ^
+            --name "IV Swinger 2" ^
+            %GITHUB%\IV_Swinger\python3\IV_Swinger2_gui.py
